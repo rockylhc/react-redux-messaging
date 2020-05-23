5118a07974a0f1d6173c58e3318dca65f1988014
Rocky <rockylhc@gmail.com>
2020-05-23 12:51:04 +0800

    feat: add gitignore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,5 +2,5 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 export default store;
